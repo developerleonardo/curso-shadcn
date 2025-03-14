39d03b40e4aa8863ec3c2550714623be20bb97d6
developerleonardo <leo.11hnz@gmail.com>
2025-03-14 15:59:12 -0500

    First commit with initial config

---
+++ src/app/dashboard/accordion/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div>
+        <h1>Accordion page</h1>
+    </div>
+  )
+}
---
+++ src/app/dashboard/alert/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div>
+        <h1>Alert page</h1>
+    </div>
+  )
+}
