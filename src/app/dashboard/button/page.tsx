1bc4ee1b325b9985fa17fe2bd4151a333ed8ebf6
developerleonardo <leo.11hnz@gmail.com>
2025-03-15 15:56:07 -0500

    Add AlertDialog component and update dashboard layout

--- src/app/dashboard/button/page.tsx
+++ src/app/dashboard/button/page.tsx
@@ -7,13 +7,13 @@ export default function Home() {
   return (
     <div className="grid grid-cols-5 gap-2">
       <Button>default</Button>
-      <Button capitalize={false}>capitalize false</Button>
       <Button variant="destructive">destructive</Button>
       <Button variant="ghost">ghost</Button>
       <Button variant="link">link</Button>
       <Button variant="outline">outline</Button>
       <Button variant="secondary">secondary</Button>
-      <Button variant="success">secondary</Button>
+      <Button variant="success">success</Button>
+      <Button capitalize={false}>capitalize false</Button>
       <Button disabled>disable</Button>
       <Button onClick={() => console.log("Hola Mundo")}>Click Me</Button>
       <Button>
