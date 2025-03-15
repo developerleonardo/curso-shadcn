9e85f539f3e73f82549e9f64c3908f6503a93d3d
developerleonardo <leo.11hnz@gmail.com>
2025-03-15 15:04:37 -0500

    Add UI components: Accordion, Alert, Button; update dashboard layout

--- src/app/dashboard/alert/page.tsx
+++ src/app/dashboard/alert/page.tsx
@@ -1,7 +1,33 @@
+import { Terminal } from "lucide-react";
+
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
+
 export default function Home() {
   return (
-    <div>
-        <h1>Alert page</h1>
+    <div className="grid gap-3">
+      <Alert>
+        <Terminal className="h-4 w-4" />
+        <AlertTitle>Heads up!</AlertTitle>
+        <AlertDescription>
+          You can add components to your app using the cli.
+        </AlertDescription>
+      </Alert>
+
+      <Alert variant="destructive">
+        <Terminal className="h-4 w-4" />
+        <AlertTitle>Heads up!</AlertTitle>
+        <AlertDescription>
+          You can add components to your app using the cli.
+        </AlertDescription>
+      </Alert>
+
+      <Alert variant="success">
+        <Terminal className="h-4 w-4" />
+        <AlertTitle>Heads up!</AlertTitle>
+        <AlertDescription>
+          You can add components to your app using the cli.
+        </AlertDescription>
+      </Alert>
     </div>
-  )
+  );
 }
---
+++ src/app/dashboard/button/page.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { ChevronRight, Loader2, MailOpen } from "lucide-react";
+
+export default function Home() {
+  return (
+    <div className="grid grid-cols-5 gap-2">
+      <Button>default</Button>
+      <Button capitalize={false}>capitalize false</Button>
+      <Button variant="destructive">destructive</Button>
+      <Button variant="ghost">ghost</Button>
+      <Button variant="link">link</Button>
+      <Button variant="outline">outline</Button>
+      <Button variant="secondary">secondary</Button>
+      <Button variant="success">secondary</Button>
+      <Button disabled>disable</Button>
+      <Button onClick={() => console.log("Hola Mundo")}>Click Me</Button>
+      <Button>
+        <MailOpen /> Login with Email
+      </Button>
+      <Button disabled>
+        <Loader2 className="animate-spin" />
+        Please wait
+      </Button>
+      <Button variant="outline" size="icon">
+        <ChevronRight />
+      </Button>
+    </div>
+  );
+}
